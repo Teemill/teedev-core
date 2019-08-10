2c54db26ef3ea5089e423e9482d39fc9d3fe233c
DanEvans485 <dan.e@teemill.com>
2019-08-10 20:39:27 +0100

    fix: .gitignore fix

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { default as Vector } from './types/vector';
-export { default as Angle } from './types/angle';
-export { default as Matrix } from './types/matrix';
-export { default as Transform } from './types/transform';
--- dist/types/transform.d.ts
+++
@@ -1,7 +0,0 @@
-import { Matrix } from '../index';
-export default class Transform extends Matrix {
-    stack: Matrix[];
-    constructor(...params: number[]);
-    push(): void;
-    pop(): this;
-}
